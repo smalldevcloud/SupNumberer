222cd856782b6e0229b021401b545944442bfd78
smalldevcloud <darkstormcloud8@gmail.com>
2022-08-15 08:25:36 +0300

    Initial Commit

---
+++ SupNumberer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SupNumberer
+//
+//  Created by cloud8 on 15.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
