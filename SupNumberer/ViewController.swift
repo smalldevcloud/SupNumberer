81a42576d7fcd5e87487a8f6b594c24f5f4a86ad
smalldevcloud <darkstormcloud8@gmail.com>
2022-08-16 10:00:24 +0300

    ui improvments, clear-button added, crash with wrong number fixed

--- SupNumberer/ViewController.swift
+++ SupNumberer/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var wrongNumberTF: UITextField!
+    
+   
+   
+    @IBAction func button(_ sender: UIButton) {
+        
+        let example = "**80"
+        var tfToString = wrongNumberTF.text!
+        let nuberArr = tfToString.components(separatedBy: " ")
+        
+        if nuberArr.count <= 2 { return } else {
+        
+        let resultNum = "\(example) \(nuberArr[1]) \(nuberArr[2])"
+        
+        resultNumber.text = resultNum
+        UIPasteboard.general.string = resultNum
+            callNumber(phoneNumber: resultNum)
+            
+        }
+    }
+    
+    @IBOutlet weak var resultNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .lightGray
+        wrongNumberTF.clearButtonMode = .always
+        
     }
-
+    
+    private func callNumber(phoneNumber: String) {
+        guard let url = URL(string: "telprompt://\(phoneNumber)"),
+            UIApplication.shared.canOpenURL(url) else {
+            return
+        }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
 
 }
 
